93c42136f5ed618ae519bcaa6b7d9e5f5b47093f
Vladimir Kukushkin <vladimir.kukushkin@meetiqm.com>
2025-07-19 15:15:58 +0300

    I don’t know why is recursion always my first thinking
    Signed-off-by: Vladimir Kukushkin <vladimir.kukushkin@meetiqm.com>

--- 1480/Sources/1480/1480.swift
+++ 1480/Sources/1480/1480.swift
@@ -3,20 +3,15 @@
 
 
 class Solution {
-    private func runningSumIteration(nums: ArraySlice<Int>, sums: [Int]) -> [Int] {
-        guard let nextNum = nums.first else {
-            return sums
-        }
-        return runningSumIteration(
-            nums: nums.dropFirst(),
-            sums: sums + [(sums.last ?? 0) + nextNum]
-        )
-    }
-
     func runningSum(_ nums: [Int]) -> [Int] {
-        guard let firstItem = nums.first else {
-            return []
+        var result: [Int] = []
+        for num in nums {
+            if let last = result.last {
+                result.append(last + num)
+            } else {
+                result.append(num)
+            }
         }
-        return runningSumIteration(nums: nums.dropFirst(), sums: [firstItem])
+        return result
     }
 }
